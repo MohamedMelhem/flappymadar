52beb873a2653a010fa1a57dca8dc486f6676d9a
Petrik Diak <diak@petrik.hu>
2026-01-07 10:16:01 +0100

    nem jo még mindig :(

--- flappymadar/MainWindow.xaml.cs
+++ flappymadar/MainWindow.xaml.cs
@@ -1,107 +1,97 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-
 using System.Windows.Threading;
 
-namespace Flappy_Bird_WPF_MOO_ICT
+namespace FlappyMadar
 {
-
 	public partial class MainWindow : Window
 	{
-
 		DispatcherTimer gameTimer = new DispatcherTimer();
 
-
 		double score;
-		int gravity = 8;
+		int gravity;
 		bool gameOver;
-		Rect flappyBirdHitBox;
+		Rect birdHitBox;
 
 		public MainWindow()
 		{
 			InitializeComponent();
 
-			gameTimer.Tick += MainEventTimer;
-			gameTimer.Interval = TimeSpan.FromMilliseconds(20);
-			StartGame();
+			Loaded += (s, e) => MyCanvas.Focus();
 
+			gameTimer.Interval = TimeSpan.FromMilliseconds(20);
+			gameTimer.Tick += GameLoop;
+
+			StartGame();
 		}
 
-		private void MainEventTimer(object sender, EventArgs e)
+		private void GameLoop(object sender, EventArgs e)
 		{
 			txtScore.Content = "Score: " + score;
 
-			flappyBirdHitBox = new Rect(Canvas.GetLeft(flappyBird), Canvas.GetTop(flappyBird), flappyBird.Width - 12, flappyBird.Height);
+			birdHitBox = new Rect(Canvas.GetLeft(flappyBird),
+								  Canvas.GetTop(flappyBird),
+								  flappyBird.Width,
+								  flappyBird.Height);
 
 			Canvas.SetTop(flappyBird, Canvas.GetTop(flappyBird) + gravity);
 
-			if (Canvas.GetTop(flappyBird) < -30 || Canvas.GetTop(flappyBird) + flappyBird.Height > 460)
+			if (Canvas.GetTop(flappyBird) < -30 || Canvas.GetTop(flappyBird) > 440)
 			{
 				EndGame();
 			}
 
-
 			foreach (var x in MyCanvas.Children.OfType<Image>())
 			{
-				if ((string)x.Tag == "obs1" || (string)x.Tag == "obs2" || (string)x.Tag == "obs3")
+				if (x.Tag == null) continue;
+
+				if ((string)x.Tag == "obj1" || (string)x.Tag == "obj2" || (string)x.Tag == "obj3")
 				{
 					Canvas.SetLeft(x, Canvas.GetLeft(x) - 5);
 
-					if (Canvas.GetLeft(x) < -100)
+					if (Canvas.GetLeft(x) < -80)
 					{
-						Canvas.SetLeft(x, 800);
-
-						score += .5;
+						Canvas.SetLeft(x, 600);
+						score += 0.5;
 					}
 
-					Rect PillarHitBox = new Rect(Canvas.GetLeft(x), Canvas.GetTop(x), x.Width, x.Height);
+					Rect pipeHitBox = new Rect(Canvas.GetLeft(x),
+											   Canvas.GetTop(x),
+											   x.Width,
+											   x.Height);
 
-					if (flappyBirdHitBox.IntersectsWith(PillarHitBox))
+					if (birdHitBox.IntersectsWith(pipeHitBox))
 					{
 						EndGame();
 					}
 				}
 
-				if ((string)x.Tag == "clouds")
+				if ((string)x.Tag == "cloud")
 				{
 					Canvas.SetLeft(x, Canvas.GetLeft(x) - 1);
 
-					if (Canvas.GetLeft(x) < -250)
+					if (Canvas.GetLeft(x) < -200)
 					{
 						Canvas.SetLeft(x, 550);
-
-						score += .5;
 					}
-
 				}
-
-
 			}
-
-
 		}
 
 		private void KeyIsDown(object sender, KeyEventArgs e)
 		{
-			if (e.Key == Key.Space)
+			if (e.Key == Key.Space && !gameOver)
 			{
-				flappyBird.RenderTransform = new RotateTransform(-20, flappyBird.Width / 2, flappyBird.Height / 2);
 				gravity = -8;
+				flappyBird.RenderTransform =
+					new RotateTransform(-20, flappyBird.Width / 2, flappyBird.Height / 2);
 			}
 
-			if (e.Key == Key.R && gameOver == true)
+			if (e.Key == Key.R && gameOver)
 			{
 				StartGame();
 			}
@@ -109,54 +99,42 @@ namespace Flappy_Bird_WPF_MOO_ICT
 
 		private void KeyIsUp(object sender, KeyEventArgs e)
 		{
-			flappyBird.RenderTransform = new RotateTransform(5, flappyBird.Width / 2, flappyBird.Height / 2);
-
 			gravity = 8;
+			flappyBird.RenderTransform =
+				new RotateTransform(10, flappyBird.Width / 2, flappyBird.Height / 2);
 		}
 
 		private void StartGame()
 		{
-			MyCanvas.Focus();
-
-			int temp = 300;
-
 			score = 0;
-
+			gravity = 8;
 			gameOver = false;
 
 			Canvas.SetTop(flappyBird, 190);
 
+			int pipeX = 400;
 			foreach (var x in MyCanvas.Children.OfType<Image>())
 			{
-				if ((string)x.Tag == "obs1")
-				{
-					Canvas.SetLeft(x, 500);
-				}
-				if ((string)x.Tag == "obs2")
-				{
-					Canvas.SetLeft(x, 800);
-				}
-				if ((string)x.Tag == "obs3")
-				{
-					Canvas.SetLeft(x, 1100);
-				}
+				if (x.Tag == null) continue;
 
-				if ((string)x.Tag == "clouds")
+				if ((string)x.Tag == "obj1" || (string)x.Tag == "obj2" || (string)x.Tag == "obj3")
 				{
-					Canvas.SetLeft(x, 300 + temp);
-					temp = 800;
+					Canvas.SetLeft(x, pipeX);
+					pipeX += 200;
 				}
 			}
 
 			gameTimer.Start();
+			MyCanvas.Focus();
 		}
 
 		private void EndGame()
 		{
-			gameTimer.Stop();
-			gameOver = true;
-			txtScore.Content += " Game Over!!! Press R to restart.";
+			if (gameOver) return;
 
+			gameOver = true;
+			gameTimer.Stop();
+			txtScore.Content += "  GAME OVER - Press R";
 		}
 	}
 }
